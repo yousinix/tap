a6b5ed8029e543f8348afb4cb6d31c9ab3c0a185
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2021-10-25 15:50:56 +0200

    refactor: add save button to settings

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:lo_form/lo_form.dart';
 
 class SettingsPage extends StatelessWidget {
   static const keySettings = "settings";
@@ -11,6 +12,7 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const pattern = "https://script.google.com/macros/s/XXXX/exec";
+    final box = Hive.box(keySettings);
 
     return Scaffold(
       appBar: AppBar(
@@ -18,29 +20,52 @@ class SettingsPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: ValueListenableBuilder<Box>(
-          valueListenable: Hive.box(keySettings).listenable(),
-          builder: (context, box, widget) {
-            return TextFormField(
-              maxLines: 4,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              initialValue: box.get(keyAppScriptUrl),
-              onChanged: (str) => box.put(keyAppScriptUrl, str),
-              validator: (str) {
-                final regex = RegExp(pattern.replaceFirst("XXXX", ".*"));
-                if (str?.isEmpty ?? true) {
-                  return "Required";
-                } else if (!regex.hasMatch(str!)) {
-                  return "Must be an App Scirpt URL";
-                } else {
-                  return null;
-                }
-              },
-              decoration: const InputDecoration(
-                labelText: "App Scirpt URL",
-                hintText: pattern,
-                errorMaxLines: 2,
+        child: LoForm(
+          initialValues: {keyAppScriptUrl: box.get(keyAppScriptUrl)},
+          submittableWhen: (state) => state.isValid,
+          onSubmit: (fields, _) async {
+            final url = fields.get(keyAppScriptUrl);
+            await box.put(keyAppScriptUrl, url);
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text("Saved successfully!"),
               ),
+            );
+            return true;
+          },
+          builder: (form) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                LoTextField(
+                  name: keyAppScriptUrl,
+                  validate: (str) {
+                    final regex = RegExp(
+                      r"^" + pattern.replaceFirst("XXXX", ".*") + r"$",
+                    );
+                    if (str?.isEmpty ?? true) {
+                      return "Required";
+                    } else if (!regex.hasMatch(str!)) {
+                      return "Must be an App Scirpt URL";
+                    } else {
+                      return null;
+                    }
+                  },
+                  props: const TextFieldProps(
+                    maxLines: 4,
+                    decoration: InputDecoration(
+                      labelText: "App Scirpt URL",
+                      hintText: pattern,
+                      errorMaxLines: 2,
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: form.submit,
+                  child: const Text("Save"),
+                ),
+              ],
             );
           },
         ),
